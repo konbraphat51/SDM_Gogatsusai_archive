524ba77d030cece6557f0e3f54d75018d89df3a5
konbraphat51 <konbraphat@gmail.com>
2024-05-21 13:46:05 +0900

    article buttons

--- src/components/Pages/TopPage/TopPage.vue
+++ src/components/Pages/TopPage/TopPage.vue
@@ -1,9 +1,16 @@
 <template>
-	<div id="TopPage">first page showing</div>
+	<div id="TopPage">
+		<ArticleButtons />
+	</div>
 </template>
 
 <script>
 export default {
 	name: "Toppage",
+	components: {
+		ArticleButtons: Vue.defineAsyncComponent(() =>
+			loadModule("src/components/Pages/TopPage/ArticleButtons.vue", options),
+		),
+	},
 }
 </script>
